77e58bf0658c6ebffe4c01c17e3c4a4637b3d4ab
scratch-technoval <technoval2014@gmail.com>
2025-11-26 10:25:31 +0100

    Best example

--- example.py
+++ example.py
@@ -1,5 +1,3 @@
-# Simple example of use
-
 import superquote
 
 encoded = superquote.toSuperQuote("Hello, world !")
@@ -11,3 +9,6 @@ encoded = superquote.toSuperQuote("This\nis\nmy\npython\nlibrary !")
 print(encoded)
 decoded = superquote.fromSuperQuote(encoded)
 print(decoded)
+
+# I use backslashes to avoid breaking the chain.
+print(superquote.fromSuperQuote("(7)\"''-*+\"*\"Hello\\0044\\ you can use this lib \\0033\\\"''-*+\"*\""))
